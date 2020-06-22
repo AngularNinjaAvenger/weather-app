96991c069b712370b8ff5e83b22758c673ab0c35
AngularNinjaAvenger <osayibishop@gmail.com>
2020-06-22 19:25:42 +0100

    created ui for applicaiton

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 import React from 'react';
-
+import "./style.css"
 function App() {
   return (
-    <div>
-      Main application
+    <div className="container">
+      <div className="wrapper">
+        <div className="header-container">
+          <div className="header-top-wrapper">
+            <h1>LONDON</h1>
+            <div className="date-wrapper">
+              <h4>18:36GMT</h4>
+            </div>
+            <h1>12*</h1>
+          </div>
+          <div className="header-bottom-wrapper">
+            <span className="reloading-timmer">Reloading in 20s</span>
+            <div className="progress-bar-container">
+              <div className="progress-bar-wrapper">
+                <div className="progress-bar">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="bottom-container">
+          <div className="weather-day-info-container">
+            <h1>MON</h1>
+            <h1>12*</h1>
+            <div className="weather-info-type-wrapper">
+              <div className="weather-image-wrapper">
+                img
+              </div>
+              <h2>{"WEATHER DESC"}</h2>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
